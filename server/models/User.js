60c5f86469fb9ff09d20db54e0608f4882df5462
Sumit Kumar <ersumitkumar45@gmail.com>
2026-05-15 19:02:00 +0530

    Fix User pre-save hook for modern Mongoose async signature

--- server/models/User.js
+++ server/models/User.js
@@ -21,13 +21,10 @@ const UserSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
+UserSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(12);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 UserSchema.methods.matchPassword = async function(enteredPassword) {
